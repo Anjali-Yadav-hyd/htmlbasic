72c7c2ff82a87b27bf79844cc4e0255168269a48
Anjali-Yadav-hyd <anjaliyadav.113019@gmail.com>
2023-09-12 10:01:24 +0530

    c16.js

---
+++ javaScript/Objects/c16.js
@@ -0,0 +1,12 @@
+// case study
+employees = [
+    {"firstName":"Sai", "lastName":"Kiran", "age":27, "salary":40000}
+    {"firstName":"Krishna", "lastName":"Kumar", "age":28, "salary":30000}
+    {"firstName":"Aryan", "lastName":"Rao", "age":29, "salary":60000}
+    {"firstName":"Ramesh", "lastName":"Kumar", "age":25, "salary":70000}
+]
+
+console.log()
+// filter
+// map
+// reduce
